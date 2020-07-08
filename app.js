d0875ffb5fe6a5536ed3bce1a7160483b979d0e8
Bobby Tazioli <bobby@Bobbys-MacBook-Pro.local>
2020-07-08 16:59:33 -0500

    added some layout / styling

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ class App extends React.Component {
   render = () => {
     return (
       <div className="container">
-        <div className="header">
-          <h1>Ram 'n Dom</h1>
+        <div className="header-container">
+          <div className="header-inner">
+            <div className="header-front">
+              <h1>Ram 'n Dom</h1>
+            </div>
+            <div className="header-back">
+              <h1>A ramen-building app by Leland Shirley & Bobby Tazioli</h1>
+            </div>
+          </div>
         </div>
         <div className="bowl">
           <img src="https://i.imgur.com/nHKnzHd.png"/>
@@ -15,8 +22,10 @@ class App extends React.Component {
         </div>
         <div className="ingredients">
           <div className="options-left">
+            <h2>Available Ingredients</h2>
           </div>
           <div className="options-right">
+            <h2>Your Ramen Bowl</h2>
           </div>
         </div>
       </div>
