2f09771c9cbe4db039204a2fadd2b6dc54c327e5
Bobby Tazioli <bobby@Bobbys-MacBook-Pro.local>
2020-07-09 09:39:50 -0500

    dunno

---
+++ models/ramen.php
@@ -0,0 +1,107 @@
+<?php
+$dbconn = null;
+if(getenv('DATABASE_URL')){
+$connectionConfig = parse_url(getenv('DATABASE_URL'));
+$host = $connectionConfig['host'];
+$user = $connectionConfig['user'];
+$password = $connectionConfig['pass'];
+$port = $connectionConfig['port'];
+$dbname = trim($connectionConfig['path'],'/');
+$dbconn = pg_connect(
+"host=".$host." ".
+"user=".$user." ".
+"password=".$password." ".
+"port=".$port." ".
+"dbname=".$dbname
+);
+} else {
+$dbconn = pg_connect("host=localhost dbname=ramenshop");
+}
+
+class Order {
+    public $id;
+    public $ingredient1;
+    public $ingredient2;
+    public $ingredient3;
+    public $ingredient4;
+    public $ingredient5;
+    public $ingredient6;
+    public $ingredient7;
+    public $ingredient8;
+
+  public function __construct($id, $ingredient1, $ingredient2, $ingredient3, $ingredient4, $ingredient5, $ingredient6, $ingredient7, $ingredient8) {
+      $this->id = $id;
+      $this->ingredient1  = $ingredient1;
+      $this->ingredient2 = $ingredient2;
+      $this->ingredient3 = $ingredient3;
+      $this->ingredient4 = $ingredient4;
+      $this->ingredient5 = $ingredient5;
+      $this->ingredient6 = $ingredient6;
+      $this->ingredient7 = $ingredient7;
+      $this->ingredient8 = $ingredient8;
+  }
+}
+
+class Orders {
+  //all Ramen function
+  static function all(){
+    $ramen = array();
+    $results = pg_query("SELECT * FROM ramen");
+
+    $order = pg_fetch_object($results);
+    while ($order) {
+      $newBowl = new Order(
+          intval($order->id),
+          $order->ingredient1,
+          $order->ingredient2,
+          $order->ingredient3,
+          $order->ingredient4,
+          $order->ingredient5,
+          $order->ingredient6,
+          $order->ingredient7,
+          $order->ingredient8,
+      );
+        $ramen[]=$newBowl;
+        $order = pg_fetch_object($results);
+    }
+    return $ramen;
+  }
+
+  static function create($order){
+    $query = "INSERT INTO ramen (ingredient1, ingredient2, ingredient3, ingredient4, ingredient5, ingredient6, ingredient7, ingredient8) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)";
+    $queryParams =array($order->ingredient1, $order->ingredient2, $order->ingredient3, $order->ingredient4, $order->ingredient5, $order->ingredient6, $order->ingredient7, $order->ingredient8);
+    pg_query_params($query, $queryParams);
+    // $queryParams = array($order->Leland, $order->CHK, $order->kombu")
+    return self::all();
+  }
+
+
+  static function update($orderUpdt) {
+    $query = "UPDATE ramen SET ingredient1=$1, ingredient2=$2, ingredient3=$3, ingredient4=$4, ingredient5=$5, ingredient6=$6, ingredient7=$7, ingredient8=$8 WHERE id=$9";
+    $queryParams = array(
+        $orderUpdt->ingredient1,
+        $orderUpdt->ingredient2,
+        $orderUpdt->ingredient3,
+        $orderUpdt->ingredient4,
+        $orderUpdt->ingredient5,
+        $orderUpdt->ingredient6,
+        $orderUpdt->ingredient7,
+        $orderUpdt->ingredient8,
+        $orderUpdt->id
+    );
+    $result = pg_query_params($query, $queryParams);
+
+    return self::all();
+  }
+
+  static function delete($id){
+    $query = "DELETE FROM ramen WHERE id = $1";
+    $queryParams = array($id);
+    $result = pg_query_params($query, $queryParams);
+
+    return self::all();
+  }
+}
+
+
+ ?>
